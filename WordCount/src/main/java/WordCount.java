bf2b1f21d391b9d4ce2fe3edce46d66c8252c98e
daniel <danek130@gmail.com>
2021-10-30 20:22:34 +0200

    projekt

--- WordCount/src/main/java/WordCount.java
+++ WordCount/src/main/java/WordCount.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -14,34 +15,36 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCount {
 
     public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable> {
-
-        private final static IntWritable one = new IntWritable(1);
-        private final Text word = new Text();
+            extends Mapper<Object, Text, Text, Text> {
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
+            if (((LongWritable) key).get() != 0) {
+                String[] line = value.toString().split(",");
+
+                String filmId = line[1];
+                String rate = line[3];
+
+                context.write(new Text(filmId), new Text(rate));
             }
         }
     }
 
     public static class IntSumReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
-        private IntWritable result = new IntWritable();
+            extends Reducer<Text, IntWritable, Text, Text> {
+        private Text result = new Text();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
+        public void reduce(Text filmId, Iterable<IntWritable> rates,
                            Context context
         ) throws IOException, InterruptedException {
             int sum = 0;
-            for (IntWritable val : values) {
+            int count = 0;
+            for (IntWritable val : rates) {
                 sum += val.get();
+                count++;
             }
-            result.set(sum);
-            context.write(key, result);
+            result.set(sum + count + "");
+            context.write(filmId, result);
         }
     }
 
@@ -54,6 +57,7 @@ public class WordCount {
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
